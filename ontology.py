7712eec5c1790f3f8d924ded8248fc2f82360079
joshuacassidy <joshcassidy79@gmail.com>
2020-12-02 18:14:03 +0000

    added starter readme

--- ontology.py
+++ ontology.py
@@ -1,15 +1,46 @@
-import rdflib
+# import rdflib
 
-g = rdflib.Graph()
-g.parse("output.ttl", format="turtle")
+# g = rdflib.Graph()
+# g.parse("output.ttl", format="turtle")
 
-qres = g.query(
-    """SELECT DISTINCT * WHERE {
-  ?s ?p ?o
-}
-LIMIT 10
-""")
+# qres = g.query(
+#     """
+# #What room types are available where there are fishing activities?
+# PREFIX HostURL:<http://www.semanticweb.org/ontologies/2020/10/Host>
+# PREFIX Activity: <http://www.semanticweb.org/ontologies/2020/10/Activity>
+# PREFIX owl:<http://www.w3.org/2002/07/owl#>
+# PREFIX rdfs:<http://www.w3.org/2007/01/rdf-schema#>
+# PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+# SELECT ?roomType ?activityLocation {
+#   ?location  <http://www.semanticweb.org/ontologies/2020/10/activityLocation> ?activityLocation .
+#   ?accomLocation <http://www.semanticweb.org/ontologies/2020/10/accommodationLocatedIn> ?accomodationLocation .
+#   ?room <http://www.semanticweb.org/ontologies/2020/10/isRoomType> ?roomTypeId .
+#   ?roomTypeId <http://www.semanticweb.org/ontologies/2020/10/roomType> ?roomType .
+#   ?activityId <http://www.semanticweb.org/ontologies/2020/10/hasActivityType> <http://foo.example/ActivitiesTypes/Fishing> .
+#   ?activityId <http://www.semanticweb.org/ontologies/2020/10/activityName> ?activity .
+# } GROUP BY ?roomType ?activityLocation
+# """)
 
 
-for row in qres:
-    print(row)
+# for row in qres:
+#     print(row)
+
+
+import requests
+response = requests.post('http://localhost:3030/test10/sparql',
+       data={'query': """
+PREFIX HostURL:<http://www.semanticweb.org/ontologies/2020/10/Host>
+PREFIX Activity: <http://www.semanticweb.org/ontologies/2020/10/Activity>
+PREFIX owl:<http://www.w3.org/2002/07/owl#>
+PREFIX rdfs:<http://www.w3.org/2007/01/rdf-schema#>
+PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+SELECT ?roomType ?activityLocation {
+  ?location  <http://www.semanticweb.org/ontologies/2020/10/activityLocation> ?activityLocation .
+  ?accomLocation <http://www.semanticweb.org/ontologies/2020/10/accommodationLocatedIn> ?accomodationLocation .
+  ?room <http://www.semanticweb.org/ontologies/2020/10/isRoomType> ?roomTypeId .
+  ?roomTypeId <http://www.semanticweb.org/ontologies/2020/10/roomType> ?roomType .
+  ?activityId <http://www.semanticweb.org/ontologies/2020/10/hasActivityType> <http://foo.example/ActivitiesTypes/Fishing> .
+  ?activityId <http://www.semanticweb.org/ontologies/2020/10/activityName> ?activity .
+} GROUP BY ?roomType ?activityLocation
+"""})
+print(response.json())
---
+++ readme.md
@@ -0,0 +1,6 @@
+run server: gunicorn -b 0.0.0.0:5000 --reload "prod:create_app()
+run database: 
+1. fuseki-server 
+2. ensure the collection is called test_project and insert the data
+
+
